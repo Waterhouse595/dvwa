048c832ad1798f767cfa9e17202b9b5f5c78a70f
lukeW <145985501+Waterhouse595@users.noreply.github.com>
2024-12-06 15:13:45 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -6,17 +6,12 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 dvwaPageStartup( array( 'authenticated' ) );
 
 $page = dvwaPageNewGrab();
-$page[ 'title' ]   = 'Vulnerability: Stored Cross Site Scripting (XSS)' . $page[ 'title_separator' ].$page[ 'title' ];
-$page[ 'page_id' ] = 'xss_s';
-$page[ 'help_button' ]   = 'xss_s';
-$page[ 'source_button' ] = 'xss_s';
+$page[ 'title' ]   = 'Vulnerability: Cross Site Request Forgery (CSRF)' . $page[ 'title_separator' ].$page[ 'title' ];
+$page[ 'page_id' ] = 'csrf';
+$page[ 'help_button' ]   = 'csrf';
+$page[ 'source_button' ] = 'csrf';
 
 dvwaDatabaseConnect();
-
-if (array_key_exists ("btnClear", $_POST)) {
-	$query  = "TRUNCATE guestbook;";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
-}
 
 $vulnerabilityFile = '';
 switch( dvwaSecurityLevelGet() ) {
@@ -34,51 +29,65 @@ switch( dvwaSecurityLevelGet() ) {
 		break;
 }
 
-require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/xss_s/source/{$vulnerabilityFile}";
+require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/csrf/source/{$vulnerabilityFile}";
+
+$testCredentials = "
+ <button onclick=\"testFunct()\">Test Credentials</button><br /><br />
+ <script>
+function testFunct() {
+  window.open(\"" . DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/csrf/test_credentials.php\", \"_blank\", 
+  \"toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=600,height=400\");
+}
+</script>
+";
 
 $page[ 'body' ] .= "
 <div class=\"body_padded\">
-	<h1>Vulnerability: Stored Cross Site Scripting (XSS)</h1>
+	<h1>Vulnerability: Cross Site Request Forgery (CSRF)</h1>
 
 	<div class=\"vulnerable_code_area\">
-		<form method=\"post\" name=\"guestform\" \">
-			<table width=\"550\" border=\"0\" cellpadding=\"2\" cellspacing=\"1\">
-				<tr>
-					<td width=\"100\">Name *</td>
-					<td><input name=\"txtName\" type=\"text\" size=\"30\" maxlength=\"10\"></td>
-				</tr>
-				<tr>
-					<td width=\"100\">Message *</td>
-					<td><textarea name=\"mtxMessage\" cols=\"50\" rows=\"3\" maxlength=\"50\"></textarea></td>
-				</tr>
-				<tr>
-					<td width=\"100\">&nbsp;</td>
-					<td>
-						<input name=\"btnSign\" type=\"submit\" value=\"Sign Guestbook\" onclick=\"return validateGuestbookForm(this.form);\" />
-						<input name=\"btnClear\" type=\"submit\" value=\"Clear Guestbook\" onClick=\"return confirmClearGuestbook();\" />
-					</td>
-				</tr>
-			</table>\n";
+		<h3>Change your admin password:</h3>
+		<br /> 
+		<div id=\"test_credentials\">
+			".$testCredentials ."
+		</div><br />
+		<form action=\"#\" method=\"GET\">";
 
-if( $vulnerabilityFile == 'impossible.php' )
+if( $vulnerabilityFile == 'impossible.php' ) {
+	$page[ 'body' ] .= "
+			Current password:<br />
+			<input type=\"password\" AUTOCOMPLETE=\"off\" name=\"password_current\"><br />";
+}
+
+$page[ 'body' ] .= "
+			New password:<br />
+			<input type=\"password\" AUTOCOMPLETE=\"off\" name=\"password_new\"><br />
+			Confirm new password:<br />
+			<input type=\"password\" AUTOCOMPLETE=\"off\" name=\"password_conf\"><br />
+			<br />
+			<input type=\"submit\" value=\"Change\" name=\"Change\">\n";
+
+if( $vulnerabilityFile == 'high.php' || $vulnerabilityFile == 'impossible.php' )
 	$page[ 'body' ] .= "			" . tokenField();
 
 $page[ 'body' ] .= "
 		</form>
 		{$html}
 	</div>
-	<br />
-
-	" . dvwaGuestbook() . "
-	<br />
+		<p>Note: Browsers are starting to default to setting the <a href='https://web.dev/samesite-cookies-explained/'>SameSite cookie</a> flag to Lax, and in doing so are killing off some types of CSRF attacks. When they have completed their mission, this lab will not work as originally expected.</p>
+		<p>Announcements:</p>
+		<ul>
+			<li><a href='https://chromestatus.com/feature/5088147346030592'>Chromium</a></li>
+			<li><a href='https://docs.microsoft.com/en-us/microsoft-edge/web-platform/site-impacting-changes'>Edge</a></li>
+			<li><a href='https://hacks.mozilla.org/2020/08/changes-to-samesite-cookie-behavior/'>Firefox</a></li>
+		</ul>
+		<p>As an alternative to the normal attack of hosting the malicious URLs or code on a separate host, you could try using other vulnerabilities in this app to store them, the Stored XSS lab would be a good place to start.</p>
 
 	<h2>More Information</h2>
 	<ul>
-		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/attacks/xss' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/xss-filter-evasion-cheatsheet' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://en.wikipedia.org/wiki/Cross-site_scripting' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.cgisecurity.com/xss-faq.html' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.scriptalert1.com/' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/attacks/csrf' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://www.cgisecurity.com/csrf-faq.html' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://en.wikipedia.org/wiki/Cross-site_request_forgery ' ) . "</li>
 	</ul>
 </div>\n";
 
