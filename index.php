71dd06c01fa1a9b1399f022b2b2d43b5c63a2133
lukeW <145985501+Waterhouse595@users.noreply.github.com>
2024-12-06 13:40:33 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -6,14 +6,18 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 dvwaPageStartup( array( 'authenticated' ) );
 
 $page = dvwaPageNewGrab();
-$page[ 'title' ]   = 'Vulnerability: SQL Injection' . $page[ 'title_separator' ].$page[ 'title' ];
-$page[ 'page_id' ] = 'sqli';
-$page[ 'help_button' ]   = 'sqli';
-$page[ 'source_button' ] = 'sqli';
+$page[ 'title' ]   = 'Vulnerability: Stored Cross Site Scripting (XSS)' . $page[ 'title_separator' ].$page[ 'title' ];
+$page[ 'page_id' ] = 'xss_s';
+$page[ 'help_button' ]   = 'xss_s';
+$page[ 'source_button' ] = 'xss_s';
 
 dvwaDatabaseConnect();
 
-$method            = 'GET';
+if (array_key_exists ("btnClear", $_POST)) {
+	$query  = "TRUNCATE guestbook;";
+	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+}
+
 $vulnerabilityFile = '';
 switch( dvwaSecurityLevelGet() ) {
 	case 'low':
@@ -21,7 +25,6 @@ switch( dvwaSecurityLevelGet() ) {
 		break;
 	case 'medium':
 		$vulnerabilityFile = 'medium.php';
-		$method = 'POST';
 		break;
 	case 'high':
 		$vulnerabilityFile = 'high.php';
@@ -31,49 +34,51 @@ switch( dvwaSecurityLevelGet() ) {
 		break;
 }
 
-require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/sqli/source/{$vulnerabilityFile}";
+require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/xss_s/source/{$vulnerabilityFile}";
 
 $page[ 'body' ] .= "
 <div class=\"body_padded\">
-	<h1>Vulnerability: SQL Injection</h1>
+	<h1>Vulnerability: Stored Cross Site Scripting (XSS)</h1>
 
-	<div class=\"vulnerable_code_area\">";
-if( $vulnerabilityFile == 'high.php' ) {
-	$page[ 'body' ] .= "Click <a href=\"#\" onclick=\"javascript:popUp('session-input.php');return false;\">here to change your ID</a>.";
-}
-else {
-	$page[ 'body' ] .= "
-		<form action=\"#\" method=\"{$method}\">
-			<p>
-				User ID:";
-	if( $vulnerabilityFile == 'medium.php' ) {
-		$page[ 'body' ] .= "\n				<select name=\"id\">";
+	<div class=\"vulnerable_code_area\">
+		<form method=\"post\" name=\"guestform\" \">
+			<table width=\"550\" border=\"0\" cellpadding=\"2\" cellspacing=\"1\">
+				<tr>
+					<td width=\"100\">Name *</td>
+					<td><input name=\"txtName\" type=\"text\" size=\"30\" maxlength=\"10\"></td>
+				</tr>
+				<tr>
+					<td width=\"100\">Message *</td>
+					<td><textarea name=\"mtxMessage\" cols=\"50\" rows=\"3\" maxlength=\"50\"></textarea></td>
+				</tr>
+				<tr>
+					<td width=\"100\">&nbsp;</td>
+					<td>
+						<input name=\"btnSign\" type=\"submit\" value=\"Sign Guestbook\" onclick=\"return validateGuestbookForm(this.form);\" />
+						<input name=\"btnClear\" type=\"submit\" value=\"Clear Guestbook\" onClick=\"return confirmClearGuestbook();\" />
+					</td>
+				</tr>
+			</table>\n";
 
-		for( $i = 1; $i < $number_of_rows + 1 ; $i++ ) { $page[ 'body' ] .= "<option value=\"{$i}\">{$i}</option>"; }
-		$page[ 'body' ] .= "</select>";
-	}
-	else
-		$page[ 'body' ] .= "\n				<input type=\"text\" size=\"15\" name=\"id\">";
+if( $vulnerabilityFile == 'impossible.php' )
+	$page[ 'body' ] .= "			" . tokenField();
 
-	$page[ 'body' ] .= "\n				<input type=\"submit\" name=\"Submit\" value=\"Submit\">
-			</p>\n";
-
-	if( $vulnerabilityFile == 'impossible.php' )
-		$page[ 'body' ] .= "			" . tokenField();
-
-	$page[ 'body' ] .= "
-		</form>";
-}
 $page[ 'body' ] .= "
+		</form>
 		{$html}
 	</div>
+	<br />
+
+	" . dvwaGuestbook() . "
+	<br />
 
 	<h2>More Information</h2>
 	<ul>
-		<li>" . dvwaExternalLinkUrlGet( 'https://en.wikipedia.org/wiki/SQL_injection' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.netsparker.com/blog/web-security/sql-injection-cheat-sheet/' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/attacks/SQL_Injection' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://bobby-tables.com/' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/attacks/xss' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://owasp.org/www-community/xss-filter-evasion-cheatsheet' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://en.wikipedia.org/wiki/Cross-site_scripting' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://www.cgisecurity.com/xss-faq.html' ) . "</li>
+		<li>" . dvwaExternalLinkUrlGet( 'https://www.scriptalert1.com/' ) . "</li>
 	</ul>
 </div>\n";
 
